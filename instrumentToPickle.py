c4e8b532ef263ca3d28136fd953971a8f01ff060
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-01-26 14:01:34 +0100

    rename pickle script

--- instrumentToPickle.py
+++ instrumentToPickle.py
@@ -1,7 +1,5 @@
 from ctapipe.io.hessio import hessio_event_source
-import astropy
 import argparse
-import numpy as np
 import pickle
 import gzip
 
@@ -9,26 +7,6 @@ parser = argparse.ArgumentParser(description='Write instrument module from event
                                              'file to json.')
 parser.add_argument('input_path', metavar='I', type=str, help='path to eventio file')
 parser.add_argument('output_path', metavar='O', type=str, help='path to pickled file')
-
-
-def serialize_dict_with_units(dct):
-    d = {}
-    for k, v in dct.items():
-        if isinstance(v, float) and np.isnan(v):
-            d[str(k)] = 'NaN'
-        elif isinstance(v, astropy.units.Quantity):
-            s = v.value
-            if isinstance(s, np.ndarray):
-                s = s.tolist()
-            d[str(k)] = {'__value__': s, '__unit__': v.unit.name}
-        elif isinstance(v, np.ndarray):
-            d[str(k)] = v.tolist()
-        elif isinstance(v, dict):
-            d[str(k)] = serialize_dict_with_units(v)
-        else:
-            d[str(k)] = v
-    return d
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
